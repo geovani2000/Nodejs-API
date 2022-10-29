106803f5dd5c3ba6351a14a82bcbbdd1019422ad
Alex <geovanialex4@gmail.com>
2022-10-29 17:37:18 -0500

    CAMBIOS

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ import { config } from 'dotenv';
 
 config()
 
-export const PORT = process.env.DB_PORT|| 3000;
+export const PORT = process.env.PORT || 3000;
+
 export const HOST = process.env.DB_HOST || 'localhost';
 export const USER = process.env.DB_USER || 'root';
 export const PASS = process.env.DB_PASS || 'geova2000';
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import zapateraRuta from './routes/zapateria.routes.js'
 import indexRuta from './routes/index.routes.js'
 import { PORT} from './config.js';
+
+
 const app = express();
 app.use(express.json())
 
